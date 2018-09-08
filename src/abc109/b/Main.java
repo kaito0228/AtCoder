04c4b76456add72e9229cae31daa40cc6327493e
ryo.s <aqua.timez.saiko@gmail.com>
2018-09-09 00:35:43 +0900

    abc109 commited

--- src/abc109/b/Main.java
+++ src/abc109/b/Main.java
@@ -7,142 +7,143 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(System.out); Scanner sc = new Scanner(System.in);) {
-            Task task = new Task();
-            task.solve(sc, out);
-            out.flush();
+  public static void main(String[] args) {
+    try (PrintWriter out = new PrintWriter(System.out);
+        Scanner sc = new Scanner(System.in)) {
+      Task task = new Task();
+      task.solve(sc, out);
+      out.flush();
 
-        } catch (Exception e) {
-            // DO NOT USE
+    } catch (Exception e) {
+      // DO NOT USE
+    }
+  }
+
+  static class Task {
+    public void solve(Scanner sc, PrintWriter out) {
+      // TODO
+      int a = nint(sc);
+
+      List<String> list = new ArrayList<>();
+      String w1 = nstr(sc);
+      list.add(w1);
+      String[] ww = w1.split("");
+
+      String stock = ww[ww.length - 1];
+
+      boolean flag = true;
+      String w = "";
+      for (int i = 0; i < a - 1; i++) {
+
+        w = nstr(sc);
+        String[] www = w.split("");
+
+        if (list.contains(w)) {
+          flag = false;
+          break;
         }
-    }
 
-    static class Task {
-        public void solve(Scanner sc, PrintWriter out) {
-            // TODO
-            int a = nint(sc);
-
-            List<String> list = new ArrayList<>();
-            String w1 = nstr(sc);
-            list.add(w1);
-            String[] ww = w1.split("");
-
-            String stock = ww[ww.length-1];
-
-            boolean flag = true;
-            String w = "";
-            for (int i = 0; i < a-1; i++){
-
-                w = nstr(sc);
-                String[] www = w.split("");
-
-                if (list.contains(w)){
-                    flag = false;
-                    break;
-                }
-
-                if (stock.equals(www[0])){
-                    list.add(w);
-                }else {
-                    flag = false;
-                    break;
-                }
-                stock = www[www.length-1];
-            }
-
-            if (flag) {
-                out.println("Yes");
-            }else {
-                out.println("No");
-            }
+        if (stock.equals(www[0])) {
+          list.add(w);
+        } else {
+          flag = false;
+          break;
         }
-    }
+        stock = www[www.length - 1];
+      }
 
-    // method
-    static int nint(Scanner sc) {
-        return Integer.parseInt(sc.next());
+      if (flag) {
+        out.println("Yes");
+      } else {
+        out.println("No");
+      }
     }
+  }
 
-    static long nlong(Scanner sc) {
-        return Long.parseLong(sc.next());
-    }
+  // method
+  static int nint(Scanner sc) {
+    return Integer.parseInt(sc.next());
+  }
 
-    static double ndouble(Scanner sc) {
-        return Double.parseDouble(sc.next());
-    }
+  static long nlong(Scanner sc) {
+    return Long.parseLong(sc.next());
+  }
 
-    static float nfloat(Scanner sc) {
-        return Float.parseFloat(sc.next());
-    }
+  static double ndouble(Scanner sc) {
+    return Double.parseDouble(sc.next());
+  }
 
-    static String nstr(Scanner sc) {
-        return sc.next();
-    }
+  static float nfloat(Scanner sc) {
+    return Float.parseFloat(sc.next());
+  }
 
-    static long[] longLine(Scanner sc, int size) {
-        long[] lLine = new long[size];
-        for (int i = 0; i < size; i++) {
-            lLine[i] = nlong(sc);
-        }
-        return lLine;
-    }
+  static String nstr(Scanner sc) {
+    return sc.next();
+  }
 
-    static int[] intLine(Scanner sc, int size) {
-        int[] iLine = new int[size];
-        for (int i = 0; i < size; i++) {
-            iLine[i] = nint(sc);
-        }
-        return iLine;
+  static long[] longLine(Scanner sc, int size) {
+    long[] lLine = new long[size];
+    for (int i = 0; i < size; i++) {
+      lLine[i] = nlong(sc);
     }
+    return lLine;
+  }
 
-    static String[] strLine(Scanner sc, int size) {
-        String[] strLine = new String[size];
-        for (int i = 0; i < size; i++) {
-            strLine[i] = nstr(sc);
-        }
-        return strLine;
+  static int[] intLine(Scanner sc, int size) {
+    int[] iLine = new int[size];
+    for (int i = 0; i < size; i++) {
+      iLine[i] = nint(sc);
     }
+    return iLine;
+  }
 
-    static long maxFromList(List<Long> longList) {
-        return longList.stream().max(Comparator.naturalOrder()).get();
+  static String[] strLine(Scanner sc, int size) {
+    String[] strLine = new String[size];
+    for (int i = 0; i < size; i++) {
+      strLine[i] = nstr(sc);
     }
+    return strLine;
+  }
 
-    static long minFromList(List<Long> longList) {
-        return longList.stream().min(Comparator.naturalOrder()).get();
-    }
+  static long maxFromList(List<Long> longList) {
+    return longList.stream().max(Comparator.naturalOrder()).get();
+  }
 
-    public static int sumDigits(int n) {
-        int sum = 0;
-        while (n != 0) {
-            sum += n % 10;
-            n /= 10;
-        }
-        return sum;
-    }
+  static long minFromList(List<Long> longList) {
+    return longList.stream().min(Comparator.naturalOrder()).get();
+  }
 
-    public static long sumDigits(long n) {
-        long sum = 0;
-        while (n != 0) {
-            sum += n % 10;
-            n /= 10;
-        }
-        return sum;
+  public static int sumDigits(int n) {
+    int sum = 0;
+    while (n != 0) {
+      sum += n % 10;
+      n /= 10;
     }
+    return sum;
+  }
 
-    static List<Integer> getIntegerList(Scanner sc, int size) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            list.add(nint(sc));
-        }
-        return list;
+  public static long sumDigits(long n) {
+    long sum = 0;
+    while (n != 0) {
+      sum += n % 10;
+      n /= 10;
     }
+    return sum;
+  }
 
-    static List<Long> getLongList(Scanner sc, int size) {
-        List<Long> list = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            list.add(nlong(sc));
-        }
-        return list;
+  static List<Integer> getIntegerList(Scanner sc, int size) {
+    List<Integer> list = new ArrayList<>();
+    for (int i = 0; i < size; i++) {
+      list.add(nint(sc));
     }
+    return list;
+  }
+
+  static List<Long> getLongList(Scanner sc, int size) {
+    List<Long> list = new ArrayList<>();
+    for (int i = 0; i < size; i++) {
+      list.add(nlong(sc));
+    }
+    return list;
+  }
 }
--- src/abc109/d/Main.java
+++ src/abc109/d/Main.java
@@ -1,121 +1,121 @@
 package abc109.d;
 
 import java.io.PrintWriter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(System.out); Scanner sc = new Scanner(System.in);) {
-            Task task = new Task();
-            task.solve(sc, out);
-            out.flush();
+  public static void main(String[] args) {
+    try (PrintWriter out = new PrintWriter(System.out);
+        Scanner sc = new Scanner(System.in)) {
+      Task task = new Task();
+      task.solve(sc, out);
+      out.flush();
 
-        } catch (Exception e) {
-            // DO NOT USE
-        }
+    } catch (Exception e) {
+      // DO NOT USE
     }
+  }
 
-    static class Task {
-        public void solve(Scanner sc, PrintWriter out) {
+  static class Task {
+    public void solve(Scanner sc, PrintWriter out) {
 
-            int h = nint(sc);
-            int w = nint(sc);
+      int h = nint(sc);
+      int w = nint(sc);
 
-            int[][] a = new int[h][w];
+      int[][] a = new int[h][w];
 
-            int stock = 0;
-            boolean flag = false;
-
-
-
-        }
+      int stock = 0;
+      boolean flag = false;
     }
+  }
 
-    // method
-    static int nint(Scanner sc) {
-        return Integer.parseInt(sc.next());
-    }
+  // method
+  static int nint(Scanner sc) {
+    return Integer.parseInt(sc.next());
+  }
 
-    static long nlong(Scanner sc) {
-        return Long.parseLong(sc.next());
-    }
+  static long nlong(Scanner sc) {
+    return Long.parseLong(sc.next());
+  }
 
-    static double ndouble(Scanner sc) {
-        return Double.parseDouble(sc.next());
-    }
+  static double ndouble(Scanner sc) {
+    return Double.parseDouble(sc.next());
+  }
 
-    static float nfloat(Scanner sc) {
-        return Float.parseFloat(sc.next());
-    }
+  static float nfloat(Scanner sc) {
+    return Float.parseFloat(sc.next());
+  }
 
-    static String nstr(Scanner sc) {
-        return sc.next();
-    }
+  static String nstr(Scanner sc) {
+    return sc.next();
+  }
 
-    static long[] longLine(Scanner sc, int size) {
-        long[] lLine = new long[size];
-        for (int i = 0; i < size; i++) {
-            lLine[i] = nlong(sc);
-        }
-        return lLine;
+  static long[] longLine(Scanner sc, int size) {
+    long[] lLine = new long[size];
+    for (int i = 0; i < size; i++) {
+      lLine[i] = nlong(sc);
     }
+    return lLine;
+  }
 
-    static int[] intLine(Scanner sc, int size) {
-        int[] iLine = new int[size];
-        for (int i = 0; i < size; i++) {
-            iLine[i] = nint(sc);
-        }
-        return iLine;
+  static int[] intLine(Scanner sc, int size) {
+    int[] iLine = new int[size];
+    for (int i = 0; i < size; i++) {
+      iLine[i] = nint(sc);
     }
+    return iLine;
+  }
 
-    static String[] strLine(Scanner sc, int size) {
-        String[] strLine = new String[size];
-        for (int i = 0; i < size; i++) {
-            strLine[i] = nstr(sc);
-        }
-        return strLine;
+  static String[] strLine(Scanner sc, int size) {
+    String[] strLine = new String[size];
+    for (int i = 0; i < size; i++) {
+      strLine[i] = nstr(sc);
     }
+    return strLine;
+  }
 
-    static long maxFromList(List<Long> longList) {
-        return longList.stream().max(Comparator.naturalOrder()).get();
-    }
+  static long maxFromList(List<Long> longList) {
+    return longList.stream().max(Comparator.naturalOrder()).get();
+  }
 
-    static long minFromList(List<Long> longList) {
-        return longList.stream().min(Comparator.naturalOrder()).get();
-    }
+  static long minFromList(List<Long> longList) {
+    return longList.stream().min(Comparator.naturalOrder()).get();
+  }
 
-    public static int sumDigits(int n) {
-        int sum = 0;
-        while (n != 0) {
-            sum += n % 10;
-            n /= 10;
-        }
-        return sum;
+  public static int sumDigits(int n) {
+    int sum = 0;
+    while (n != 0) {
+      sum += n % 10;
+      n /= 10;
     }
+    return sum;
+  }
 
-    public static long sumDigits(long n) {
-        long sum = 0;
-        while (n != 0) {
-            sum += n % 10;
-            n /= 10;
-        }
-        return sum;
+  public static long sumDigits(long n) {
+    long sum = 0;
+    while (n != 0) {
+      sum += n % 10;
+      n /= 10;
     }
+    return sum;
+  }
 
-    static List<Integer> getIntegerList(Scanner sc, int size) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            list.add(nint(sc));
-        }
-        return list;
+  static List<Integer> getIntegerList(Scanner sc, int size) {
+    List<Integer> list = new ArrayList<>();
+    for (int i = 0; i < size; i++) {
+      list.add(nint(sc));
     }
+    return list;
+  }
 
-    static List<Long> getLongList(Scanner sc, int size) {
-        List<Long> list = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            list.add(nlong(sc));
-        }
-        return list;
+  static List<Long> getLongList(Scanner sc, int size) {
+    List<Long> list = new ArrayList<>();
+    for (int i = 0; i < size; i++) {
+      list.add(nlong(sc));
     }
-    
+    return list;
+  }
 }
